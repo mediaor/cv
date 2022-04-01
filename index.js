efb2c6bdeecb2bde9ced18775f343e8e285fbdd6
mediaor <87041274+mediaor@users.noreply.github.com>
2022-04-01 12:32:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var greeting;
 
 if (hourNow > 18) {
     greeting = 'Good evening!';
-} else if (hourNow > 12) {
+} else if (hourNow > 11) {
     greeting = 'Good afternoon!';
 } else if (hourNow > 0) {
     greeting = 'Good morning!';
